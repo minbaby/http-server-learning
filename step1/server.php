72eebf550469ffa28f2ce5d1c9778b5dfdde8fc3
minbaby.zhang <zhangshaomin_1990@126.com>
2017-02-23 18:12:36 +0800

    熟悉中

---
+++ step1/server.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * 基本思路
+ *
+ * 需要一个死循环处理连接请求
+ *
+ * 循环中处理请求数据，响应数据
+ *
+ * 所以： 这是 单进程+阻塞
+ *
+ */
+
+
+define("HTTP_IP", "127.0.0.1");     // 绑定 ip
+define("HTTP_PORT", "9876");        // 端口
+define("PACKET_SIZE", 1500);
+
+
+//var_dump(stream_get_transports());die();
+
+$url = sprintf("tcp://%s:%s", HTTP_IP, HTTP_PORT);
+
+while (true) {
+    echo date("Y-m-d H:i:s"), PHP_EOL;
+    echo $url, PHP_EOL;
+
+    $errno = $errstr = null;
+    $server = stream_socket_server($url, $errno, $errstr);
+
+    if (!$server) {
+        logConsole(sprintf("%s (%s)", $errstr, $errno));
+        die(255);
+    }
+
+    while ($socket = stream_socket_accept($server)) {
+        $str = "";
+        $str = stream_socket_recvfrom($socket, PACKET_SIZE);
+        logConsole($str);
+
+        $ss = <<<EOF
+HTTP/1.1 200 OK
+Content-Type: text/html; charset=utf-8
+EOF;
+
+        fwrite($socket, $ss, strlen($ss));
+
+        fclose($socket);
+    }
+
+    fclose($server);
+}
+
+function logConsole($msg)
+{
+    echo sprintf("[%s] %s %s", date("Y-m-d H:i:s"), $msg, PHP_EOL);
+}
