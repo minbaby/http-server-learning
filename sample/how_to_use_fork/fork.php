368abe88cbd3bd9218505def4a3fceef443ebaac
minbaby.zhang <zhangshaomin_1990@126.com>
2017-03-06 20:01:58 +0800

    跟系统没直接关系， 似乎是需要不断调用 （pcntl_signal_dispatch） 才行

--- sample/how_to_use_fork/fork.php
+++ sample/how_to_use_fork/fork.php
@@ -2,6 +2,7 @@
 
 // 注意不同系统下，相同常量对应的数字是不一样的。
 // 这里处理 fork 一个子进程， 然后由父进程处理进程。
+// kill -s USR1 `ps -ef | grep parent | awk '{print $2}' | head -n 1`
 
 
 $signs = [
@@ -18,6 +19,7 @@ function register()
     foreach ($signs as $k => $v) {
         pcntl_signal($k, "sig_handler");
     }
+    pcntl_signal_dispatch();
 }
 
 
@@ -33,23 +35,27 @@ if ($pid === 0) {
     echo "子", "-pid-", posix_getpid(), PHP_EOL;
 
     $pids[] = posix_getpid();
-    @cli_set_process_title("children"); // osx 不支持
-    register();
+    @cli_set_process_title("php-fork-children"); // osx 不支持
 
     while (true) {
-        echo 'do-', time(), PHP_EOL;
+//        echo 'do-', time(), PHP_EOL;
 //        usleep(500 * 1000);
-        sleep(1);
-        pcntl_alarm(1);
-        posix_kill(posix_getpid(), SIGUSR1);
+        sleep(20);
         pcntl_signal_dispatch();
     }
 } elseif ($pid === -1) {
     echo 'ERROR', pcntl_get_last_error();
 } else {
-    @cli_set_process_title("parent"); // osx 不支持
+    @cli_set_process_title("php-fork-parent"); // osx 不支持
     echo "父", "-pid-", posix_getpid(), PHP_EOL;
     $status = "";
+    register();
+    while (true) {
+        //        echo 'do-', time(), PHP_EOL;
+        //        usleep(500 * 1000);
+        sleep(10);
+        pcntl_signal_dispatch();
+    }
     pcntl_wait($status); // 需要等待子进程， 防止子进程变成僵尸进程。
     exit;
 }
