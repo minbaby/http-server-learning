87b681fa720f8da254af7ede919f315b56aa7214
minbaby.zhang <zhangshaomin_1990@126.com>
2017-03-14 14:57:24 +0800

    close client

--- sample/how_to_use_event_loop/client.php
+++ sample/how_to_use_event_loop/client.php
@@ -6,5 +6,6 @@ while (true) {
     $client = stream_socket_client("tcp://localhost:987" . ($i % 2 == 0 ? 5 : 6));
     stream_socket_sendto($client, sprintf("[%5d]", $i));
     $i++;
+    fclose($client);
     usleep(1000 * 500);
 }
